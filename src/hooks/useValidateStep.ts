a7f47df4c67cb0c04204a2ae45983c9336705d93
br0bertinus <rschwartz89@gmail.com>
2026-03-03 00:33:44 -0500

    rm unused import

--- src/hooks/useValidateStep.ts
+++ src/hooks/useValidateStep.ts
@@ -1,6 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
 import { validateStep } from '../api/game';
-import type { ValidateStepResponse } from '../types';
 
 type ValidateStepArgs = {
   currentActorId: number;
